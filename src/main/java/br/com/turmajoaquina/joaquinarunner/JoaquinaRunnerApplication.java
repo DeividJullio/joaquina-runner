c4c3f8bea35ed161dcd139584bba2d5274f7f1a5
DeividJullio <120142530+DeividJullio@users.noreply.github.com>
2023-01-26 20:12:05 -0300

    firt commit

---
+++ src/main/java/br/com/turmajoaquina/joaquinarunner/JoaquinaRunnerApplication.java
@@ -0,0 +1,13 @@
+package br.com.turmajoaquina.joaquinarunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaquinaRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaquinaRunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/turmajoaquina/joaquinarunner/JoaquinaRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.turmajoaquina.joaquinarunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoaquinaRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
